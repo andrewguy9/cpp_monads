606e9a56961ed21c8db3f4f43fdcc06e8964793b
Andrew Thomson <anthomso@adobe.com>
2017-11-30 17:55:06 -0800

    Add calc application.

---
+++ calc.cpp
@@ -0,0 +1,71 @@
+#include "calc.hpp"
+
+#include<string>
+#include<vector>
+#include<map>
+#include<utility>
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+  vector<VALUE> stack;
+  for (int i = 1; i < argc; i++) {
+    string s = argv[i];
+    if (0 == s.compare("+")) {
+      //TODO would be nice to have an abstraction for application.
+      PLUS p(stack[0],stack[1]);
+      stack.pop_back();
+      stack.pop_back();
+      VALUE r = fromJust(p.eval());
+      stack.push_back(r);
+    } else if (0 == s.compare("-")) {
+      //TODO would be nice to have an abstraction for application.
+      SUB b(stack[0],stack[1]);
+      stack.pop_back();
+      stack.pop_back();
+      VALUE r = fromJust(b.eval());
+      stack.push_back(r);
+    } else if (s == "*") {
+      //TODO would be nice to have an abstraction for application.
+      MULT m(stack[0],stack[1]);
+      stack.pop_back();
+      stack.pop_back();
+      VALUE r = fromJust(m.eval());
+      stack.push_back(r);
+    } else if (s == "/") {
+      //TODO would be nice to have an abstraction for application.
+      DIV d(stack[0],stack[1]);
+      stack.pop_back();
+      stack.pop_back();
+      VALUE r = fromJust(d.eval());
+      stack.push_back(r);
+    } else if (s == "_") {
+      //TODO would be nice to have an abstraction for application.
+      NEG n(stack[0]);
+      stack.pop_back();
+      VALUE r = fromJust(n.eval());
+      stack.push_back(r);
+    } else {
+      try {
+        VALUE v(stoi(s));
+        stack.push_back(v);
+      } catch (invalid_argument) {
+        cout << "caught invalid arg:" << s << endl;
+        return 1;
+      }
+    }
+  }
+
+  if (stack.size() > 1) {
+    cout << "Stack has more than one element" << endl;
+    return 2;
+  } else if (stack.size() == 0) {
+    cout << "Stack is empty!" << endl;
+  } else {
+    VALUE first = stack[0];
+    cout << fromJust(first.eval()) << endl;
+    return 0;
+  }
+}
