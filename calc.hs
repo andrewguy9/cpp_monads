136bccb22f16f86e9473e57d5b1d92869aa14d4e
Andrew Thomson <anthomso@adobe.com>
2017-12-04 09:50:26 -0800

    Added _ + - * operators.

--- calc.hs
+++ calc.hs
@@ -4,20 +4,50 @@ import Text.Read
 import Data.Maybe
 import Control.Monad
 
-safediv :: Int -> Int -> Maybe Int
-safediv n m = if m == 0
+safeNeg :: Int -> Maybe Int
+safeNeg x = Just $ -1 * x
+
+safePlus :: Int -> Int -> Maybe Int
+safePlus x y = Just $ x+y
+
+safeSub :: Int -> Int -> Maybe Int
+safeSub x y = Just $ x-y
+
+safeMult :: Int -> Int -> Maybe Int
+safeMult x y = Just $ x*y
+
+safeDiv :: Int -> Int -> Maybe Int
+safeDiv n m = if m == 0
   then Nothing
   else Just (div n m)
 
---TODO Add other operators
-data Expr = Val Int | Div Expr Expr deriving(Show)
+data Expr =
+    Val Int         |
+    Neg Expr        |
+    Plus Expr Expr  |
+    Sub Expr Expr   |
+    Mult Expr Expr  |
+    Div Expr Expr
+  deriving(Show)
+
+apply1 :: (Int -> Maybe Int) -> Expr -> Maybe Int
+apply1 f x = do
+  resultx <- eval x
+  f resultx
+
+apply2 :: (Int -> Int -> Maybe Int) -> Expr -> Expr -> Maybe Int
+apply2 f x y = do
+  resultX <- eval x
+  resultY <- eval y
+  f resultX resultY
 
 eval :: Expr -> Maybe Int
-eval (Val n) = Just n
---TODO use `do`
-eval (Div x y) = eval x >>= (\ n ->
-                  eval y >>= (\ m ->
-                    safediv n m))
+eval (Val  n)   = Just n
+eval (Neg  x  ) = apply1 safeNeg  x
+eval (Plus x y) = apply2 safePlus x y
+eval (Sub  x y) = apply2 safeSub  x y
+eval (Mult x y) = apply2 safeMult x y
+eval (Div  x y) = apply2 safeDiv  x y
 
 readMaybeInt :: String -> Maybe Int
 readMaybeInt s = readMaybe s
@@ -25,26 +55,84 @@ readMaybeInt s = readMaybe s
 tryParseValue :: String -> Maybe Expr
 tryParseValue s = fmap Val (readMaybeInt s)
 
-tryParseDiv :: Expr -> Expr -> String -> Maybe Expr
-tryParseDiv left right s = if s == "/"
-  then Just $ Div left right else Nothing
+tryParseStr1 :: (Expr -> Expr) -> String -> Expr -> String -> Maybe Expr
+tryParseStr1 constructor opStr right token =
+  if token == opStr then
+    Just $ constructor right
+  else
+    Nothing
 
-fold_value :: [Expr] -> String -> Maybe [Expr]
-fold_value stack input = do
-  v <- tryParseValue input
+tryParseStr2 :: (Expr -> Expr -> Expr) -> String -> Expr -> Expr -> String -> Maybe Expr
+tryParseStr2 constructor opStr left right token =
+  if token == opStr then
+    Just $ constructor left right
+  else
+    Nothing
+
+tryParseNeg :: Expr -> String -> Maybe Expr
+tryParseNeg right token = tryParseStr1 Neg "_" right token
+
+tryParsePlus :: Expr -> Expr -> String -> Maybe Expr
+tryParsePlus left right token = tryParseStr2 Plus "+" left right token
+
+tryParseSub :: Expr -> Expr -> String -> Maybe Expr
+tryParseSub left right token = tryParseStr2 Sub "-" left right token
+
+tryParseMult :: Expr -> Expr -> String -> Maybe Expr
+tryParseMult left right token = tryParseStr2 Mult "*" left right token
+
+tryParseDiv :: Expr -> Expr -> String -> Maybe Expr
+tryParseDiv left right token = tryParseStr2 Div "/" left right token
+
+fold0 :: (String -> Maybe Expr) -> [Expr] -> String -> Maybe [Expr]
+fold0 tryParse stack token = do
+  v <- tryParse token
   return $ v : stack
 
-fold_div :: [Expr] -> String -> Maybe [Expr]
-fold_div stack input = do
+fold1 :: (Expr -> String -> Maybe Expr) -> [Expr] -> String -> Maybe [Expr]
+fold1 tryParse stack token = do
+  right <- listToMaybe stack
+  let remaining = tail stack
+  v <- tryParse right token
+  return $ v : remaining
+
+fold2 :: (Expr -> Expr -> String -> Maybe Expr) -> [Expr] -> String -> Maybe [Expr]
+fold2 tryParse stack token = do
   right <- listToMaybe stack
   left <- listToMaybe (tail stack)
   let remaining = (tail.tail) stack
-  v <- tryParseDiv left right input
+  v <- tryParse left right token
   return $ v : remaining
+
+fold_value :: [Expr] -> String -> Maybe [Expr]
+fold_value stack token = fold0 tryParseValue stack token
+
+fold_neg :: [Expr] -> String -> Maybe [Expr]
+fold_neg stack token = fold1 tryParseNeg stack token
+
+fold_plus :: [Expr] -> String -> Maybe [Expr]
+fold_plus stack token = fold2 tryParsePlus stack token
+
+fold_sub :: [Expr] -> String -> Maybe [Expr]
+fold_sub stack token = fold2 tryParseSub stack token
+
+fold_mult :: [Expr] -> String -> Maybe [Expr]
+fold_mult stack token = fold2 tryParseMult stack token
+
+fold_div :: [Expr] -> String -> Maybe [Expr]
+fold_div stack token = fold2 tryParseDiv stack token
 
 build_somthing :: [Expr] -> String -> Maybe [Expr]
 build_somthing stack input =
   (fold_value stack input)
+  `mplus`
+  (fold_neg stack input)
+  `mplus`
+  (fold_plus stack input)
+  `mplus`
+  (fold_sub stack input)
+  `mplus`
+  (fold_mult stack input)
   `mplus`
   (fold_div stack input)
 
@@ -58,15 +146,15 @@ parse tokens = do
   exprStack <- foldM build_somthing [] tokens
   maybeFromSingleList exprStack
 
-calc :: [String] -> Maybe Int
-calc args = do
+run :: [String] -> Maybe Int
+run args = do
   expr <- parse args
   result <- eval expr
   return result
 
 main = do
   args <- getArgs
-  let maybe_result = calc args
+  let maybe_result = run args
   putStrLn $ maybe "ERROR" show maybe_result
   if isJust maybe_result then
     exitFailure
