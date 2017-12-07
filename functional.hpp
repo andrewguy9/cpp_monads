7e9a38dfa8e580ba042f77db286e8e3059626bf7
Andrew Thomson <anthomso@adobe.com>
2017-12-07 10:12:31 -0800

    Added curry implementation.

--- functional.hpp
+++ functional.hpp
@@ -1,14 +1,25 @@
 #ifndef FUNCTIONAL_HPP
 #define FUNCTIONAL_HPP
 
+#include<functional>  // for std::function
+#include<utility> // for std::pair
+
 /* curry :: ((a, b) -> c) -> a -> b -> c */
-//May not be useful in C++ where we dont' have curring anyway.
+// May not be useful in C++ where we dont' have curring anyway.
+template <class A, class B, class C>
+std::function< std::function<C (B)> (A)>
+curry(std::function<C (std::pair<A,B>)> f) {
+  return [f] (A a) { // std::function< std::function<C (B)> (A)>
+    return [f,a] (B b) { // std::function<C (B)>
+      return f(std::pair<A,B>(a,b)); // C
+    };
+  };
+}
 
 /* uncurry :: (a -> b -> c) -> (a, b) -> c */
-#include<functional>
-#include<utility>
 template <class A, class B, class C>
-std::function<C (std::pair<A,B>)> uncurry(std::function<C (A,B)> f) {
+std::function<C (std::pair<A,B>)>
+uncurry(std::function<C (A,B)> f) {
     return [f](std::pair<A,B> i) {
       return f(i.first, i.second);
     };
--- functional_tests.cpp
+++ functional_tests.cpp
@@ -11,5 +11,9 @@ int main()
 {
   auto divide_uncurry = uncurry<int,int,int>(divide);
   assert(divide_uncurry(pair<int,int>(8,2)) == divide(8,2));
+
+  auto divide_restored = curry<int,int,int>(divide_uncurry);
+  assert(4 == divide_restored(8)(2));
+
   return 0;
 }
